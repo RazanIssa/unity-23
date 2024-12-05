83ebf4da1d79e257de75d8b14576ef37dfe4cba2
Razan <razan.2.7.2003@gmail.com>
2024-12-05 17:14:33 +0200

    ass 24

---
+++ Assets/scripts/class 16/RacingGame.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacingGame : MonoBehaviour
+{
+    public RaceState raceState;
+
+    public void SimulateRace()
+    {
+        switch (raceState)
+        {
+            case RaceState.Start:
+                Debug.Log("The race is about to begin. Get ready!");
+                break;
+
+            case RaceState.Accelerate:
+                Debug.Log("you press the gas pedal");
+                break;
+            case RaceState.Turn:
+                Debug.Log("You approach a sharp turn. Be careful not to crash!");
+                break;
+
+            case RaceState.Crash:
+                Debug.Log("You hit a barrier. The race is over.");
+                break;
+
+            case RaceState.Finish:
+                Debug.Log("You crossed the finish line. Well done!");
+                break;
+
+            default:
+                Debug.Log("This state is not defined, something went wrong");
+                break;
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SimulateRace();
+
+    }
+}
